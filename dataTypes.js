906d29465a68674261a9e04ec9e0159deb2495d1
muhammadusama-afzal <125508361+muhammadusama-afzal@users.noreply.github.com>
2023-10-04 04:58:27 +0000

    data Types in javascript

---
+++ dataTypes.js
@@ -0,0 +1,15 @@
+// Data Types In JavaScript
+// In javaSctipy there are many types of data types
+
+// var let const
+// var and let is a variable and const is used to create constant
+/* 
+number,
+bigInt
+string,
+boolean,
+symbol,
+object,
+undefiend,
+null
+*/
